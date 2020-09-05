f3d24d0bb69d262886d998d5e4f41b8f489eedae
Himanshu Mukhedkar <himanshu.mukhedkar@tudip.com>
2020-09-05 22:15:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+​
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  teams$: Observable<any> = this.http.get('/api/teams');
+​
+  constructor(private http: HttpClient) {}
 }
